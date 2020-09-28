e7588945c983662e123003093b25c4471f8b07d7
Mike Wilks <mike@wilks.org>
2020-09-28 10:25:19 +0100

    Initial quick and dirty version of the slack notify function, more tidying and updating of readme to do

--- simple-pb-monitor.py
+++ simple-pb-monitor.py
@@ -2,18 +2,45 @@ import requests
 import time
 import sys
 import os
+import ssl
+import json
+from slack import WebClient
+from slack.errors import SlackApiError
+
+
+def notify(message_to_notify):
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    client = WebClient(token=slack_token, ssl=ssl_context)
+    try:
+        response = client.chat_postMessage(
+            channel=slack_channel,
+            text=message_to_notify)
+        assert response["message"]["text"] == message
+    except SlackApiError as e:
+        assert e.response["ok"] is False
+        assert e.response["error"]
+        print(f"Error from Slack API: {e.response['error']}")
 
 # Check for command line parameters for the keywords file and output directory
 # Start with defaults of ./keywords.txt and .
 
 # Start with defaults
 keyword_file = 'keywords.txt'
+slack_file_name = 'slack.json'
 output_path = '.'
+input_path = '.'
 check_ip = False
+slack_notify = False
+slack_token = ''
+slack_channel = ''
+
 
 # keywords file as the first argument after the python file
+# Note this is changed in to input path not file in the slack notify changes
 if len(sys.argv) > 1:
-    keyword_file = sys.argv[1]
+    input_path = sys.argv[1]
 
 # output directory as the second parameter after the python file
 if len(sys.argv) > 2:
@@ -25,10 +52,21 @@ if len(sys.argv) > 3:
         check_ip = True
 
 # Load the keywords
-with open(keyword_file) as f:
+with open(os.path.join(input_path, keyword_file)) as f:
     keywords = f.read().splitlines()
 
 print("keywords ", keywords)
+
+# if slack.json exists the we'll set up notifications
+# load parameters from file
+slack_file_path = os.path.join(input_path, slack_file_name)
+if os.path.isfile(slack_file_path):
+    slack_file = open(slack_file_path, "r")
+    slack_params = json.loads(slack_file.read())
+    slack_file.close()
+    slack_notify = True
+    slack_token = slack_params["slack_bot_key"]
+    slack_channel = slack_params["channel"]
 
 check_index = 0
 check_list = []
@@ -62,7 +100,8 @@ while True:
                     # loop through the keywords to see if they are in the post
                     for word in keywords:
                         if word.lower() in text.lower():
-                            print('Matched keyword \'{}\' and will save {}'.format(word, individual['key']))
+                            message = f"Matched keyword {word} and will save {individual['key']}"
+                            print(message)
 
                             # Check whether the directory with the name of the keyword exists and create it if not
                             if not os.path.isdir(output_path+'/'+word):
@@ -73,6 +112,10 @@ while True:
                             file_object = open(output_path+'/'+word+'/'+individual['key'], 'w', encoding="utf-8")
                             file_object.write(text)
                             file_object.close()
+
+                            # Notify to slack if enabled
+                            if slack_notify:
+                                notify(message)
 
                             # Removed the break because we do want to save multiple times if multiple keywords are
                             # matched because we now have a directory per key word '''
@@ -93,3 +136,6 @@ while True:
     # wait a minute
     print("Sleeping a minute")
     time.sleep(60)
+
+
+
